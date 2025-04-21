400a8a0196e78203fac7071ae065634e258436dc
Xinran1205 <id19037@bristol.ac.uk>
2025-04-21 17:32:40 +0100

    1

--- backend/gym-pojo/src/main/java/com/gym/dto/AvailabilitySlotDTO.java
+++ backend/gym-pojo/src/main/java/com/gym/dto/AvailabilitySlotDTO.java
@@ -1,5 +1,6 @@
 package com.gym.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
@@ -13,8 +14,11 @@ import java.time.LocalDateTime;
 public class AvailabilitySlotDTO {
     private Long availabilityId;
     // @NotNull(message = "Start time cannot be null")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startTime;
 
     // @NotNull(message = "End time cannot be null")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
+
 }
